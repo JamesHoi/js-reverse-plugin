3657bbf41072540ed7ab987df5adf00a606df3b1
JamesHoi <james352053767@gmail.com>
2021-01-11 01:39:02 +0800

    Update plugin

--- js/url_setting.js
+++ js/url_setting.js
@@ -1,6 +1,17 @@
 var ReResMap = [
+	// example 1
 	{
-		"req": "https://xxxxxxxx/xxxxx.js",// The js that you want to reverse 
-		"res": "chrome-extension://"+chrome.runtime.id+"/js/xxxxx.js"// Your js
+		"req": "https://xxxxxxxx/xxxxx1.js",// The js that you want to reverse 
+		"res": "chrome-extension://"+chrome.runtime.id+"/js/xxxxx1.js"// Your js file which will save in plugin
+	},
+	// example 2
+	{
+		"req": "https://xxxxxxxx/xxxxx2.js",// The js that you want to reverse 
+		"res": "chrome-extension://"+chrome.runtime.id+"/js/xxxxx2.js"// Your js file which will save in plugin
+	},
+	// example
+	{
+		"req": "https://jameshoi.github.io/files/js-reverse/login.js",
+		"res": "chrome-extension://"+chrome.runtime.id+"/js/example.js"
 	}
 ];
